e4e21516e1ac471e163979d9b50579cc2af52a9a
dingdinglz <1149978856@qq.com>
2025-03-26 16:03:30 +0800

    ✨ feat(version): updated to v0.1.1

--- global/vars.go
+++ global/vars.go
@@ -3,7 +3,7 @@ package global
 import "backend-generator/types"
 
 var (
-	Version         string                       = "v0.1.2"
+	Version         string                       = "v0.1.1"
 	WorkSpace       string                       = ""
 	WorkSpaceConfig types.BackendGeneratorConfig = types.BackendGeneratorConfig{}
 )
